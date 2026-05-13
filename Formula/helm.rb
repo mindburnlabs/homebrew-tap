3ef38e790d20f0e4f7ae40b2098700a9bf9b514c
mindburnlabs <mindburnlabs@gmail.com>
2026-05-13 12:31:27 +0300

    Update helm formula to v0.5.0

--- Formula/helm.rb
+++ Formula/helm.rb
@@ -3,26 +3,26 @@
 class Helm < Formula
   desc "Fail-closed execution firewall for AI agents"
   homepage "https://github.com/Mindburn-Labs/helm-oss"
-  version "0.4.0"
+  version "0.5.0"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Mindburn-Labs/helm-oss/releases/download/v0.4.0/helm-darwin-arm64"
-      sha256 "24ea86a97967a6ab66ce2bee5b1f4c3b0aa47a0c87c1e4ae99af46234e7e0271"
+      url "https://github.com/Mindburn-Labs/helm-oss/releases/download/v0.5.0/helm-darwin-arm64"
+      sha256 "1036b1c1b3ef0dfa36788cd1e9ff18524da3d6cb4221ffe1a726402953a22da4"
     else
-      url "https://github.com/Mindburn-Labs/helm-oss/releases/download/v0.4.0/helm-darwin-amd64"
-      sha256 "3b895fd4220879146676fed8ffb3760e66e34fcad95ae1924347435feab88ab6"
+      url "https://github.com/Mindburn-Labs/helm-oss/releases/download/v0.5.0/helm-darwin-amd64"
+      sha256 "1b4787ca36011455395c71b012963a55deef21790d8ed59fa68381807c46ee8d"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/Mindburn-Labs/helm-oss/releases/download/v0.4.0/helm-linux-arm64"
-      sha256 "a88796fe449b2d922bc5337bd1e0cce72d26af1762b32baaa8e1dcceaaebb898"
+      url "https://github.com/Mindburn-Labs/helm-oss/releases/download/v0.5.0/helm-linux-arm64"
+      sha256 "c073f8b95e12f524d39a59648ba6a4097d1225ea83305ad8e405a757d1d338aa"
     else
-      url "https://github.com/Mindburn-Labs/helm-oss/releases/download/v0.4.0/helm-linux-amd64"
-      sha256 "647117824825416b4d1e527621fdf0340d7cc88a13254a260b6f6dc6a8ff2841"
+      url "https://github.com/Mindburn-Labs/helm-oss/releases/download/v0.5.0/helm-linux-amd64"
+      sha256 "0877a48a905e33fab87a379201b051433108e09ce993d8e8bea51430e6bc920d"
     end
   end
 
